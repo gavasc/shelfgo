48f111549d154355d154f54aa5a366327f06a847
Gabriel Vasconcelos <gavascdev@gmail.com>
2025-07-23 10:36:34 -0300

    filtering file extensions through config

--- main.go
+++ main.go
@@ -4,6 +4,10 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"path/filepath"
+	"shelfgo/config"
+	"slices"
+	"strings"
 
 	"golang.org/x/net/webdav"
 )
@@ -13,6 +17,11 @@ func main() {
 	pass := os.Getenv("SHELFGO_PASS")
 	serveDir := os.Getenv("SHELFGO_DIR")
 	port := os.Getenv("SHELFGO_PORT")
+
+	conf, err := config.Load()
+	if err != nil {
+		log.Fatal("Error loading config file: ", err)
+	}
 
 	if err := ensureDir(serveDir); err != nil {
 		log.Fatal("Failed to create directory: ", err)
@@ -33,11 +42,17 @@ func main() {
 			return
 		}
 
+		if r.Method == "PUT" && !extensionAllowed(r.URL.Path, conf.AllowedFormats) {
+			log.Print("Request from ", r.Host, " failed: file extension not allowed")
+			http.Error(w, "Forbidden: File extension not allowed", http.StatusForbidden)
+			return
+		}
+
 		handler.ServeHTTP(w, r)
 	})
 
 	log.Printf("Starting ShelfGo server on port %s, serving files from %s", port, serveDir)
-	err := http.ListenAndServe(":"+port, handler)
+	err = http.ListenAndServe(":"+port, handler)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -49,4 +64,10 @@ func ensureDir(dir string) error {
 	}
 
 	return nil
+}
+
+func extensionAllowed(path string, allowed []string) bool {
+	ext := strings.ToLower(filepath.Ext(path))
+
+	return slices.Contains(allowed, ext)
 }
